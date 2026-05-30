68f1af175c9df7e93773e6f582fe2826da900d35
Olenka Alisson Gonzales Vásquez <olenkagonzalesvasquez@gmail.com>
2026-05-29 21:01:14 -0500

    Avance 01 login dashboard y modulo colores

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\admin\AdminController;
+use App\Http\Controllers\admin\VehicleColorController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+
+    Route::get('/dashboard', [AdminController::class, 'index'])
+        ->name('dashboard');
+
+    Route::get('/admin', [AdminController::class, 'index'])
+        ->name('admin.index');
+    
+    Route::resource('admin/colors', VehicleColorController::class)
+    ->names('admin.colors');
+
 });
