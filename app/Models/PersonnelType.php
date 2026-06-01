5f13fe4aa1647113e93f15f310a559d28e25296d
Willy0509 <dambert05@gmail.com>
2026-06-01 14:13:13 -0500

    tipo de personal

--- app/Models/PersonnelType.php
+++ app/Models/PersonnelType.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
 
 class PersonnelType extends Model
 {
@@ -11,16 +10,4 @@ class PersonnelType extends Model
         'name',
         'description'
     ];
-=======
-use Illuminate\Database\Eloquent\Relations\HasMany;
-
-class PersonnelType extends Model
-{
-    protected $fillable = ['name', 'description'];
-
-    public function personnels(): HasMany
-    {
-        return $this->hasMany(Personnel::class);
-    }
->>>>>>> 76a4a5d0d367ab21a6f6c631fa175b4609069bb5
 }
